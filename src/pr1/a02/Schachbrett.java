4f9544e9b8b806783a1214647d06bcd75828e1d8
Mio <miostarkid@gmail.com>
2014-11-09 22:29:18 +0100

    Optimization

--- src/pr1/a02/Schachbrett.java
+++ src/pr1/a02/Schachbrett.java
@@ -3,38 +3,38 @@ package pr1.a02;
 import java.io.PrintWriter;
  
 public class Schachbrett {
- 
+
 	public static void main(String[] args) {
 		PrintWriter out = new PrintWriter(System.out, true);
 		printSchachbrett(out);
-		printSchachbrettReverse();
-		printSchachbrett(out);
+		printSchachbrettReverse(out);
+		printSchachbrett();
 		printSchachbrettReverse();
 	}
-	
+
 	public static void printSchachbrett(){
 		PrintWriter out = new PrintWriter(System.out, true);
 		buildChecker('A', 'H', 8, out);
 	}
-	
-	
+
+
 	public static void printSchachbrettReverse(){
 		PrintWriter out = new PrintWriter(System.out, true);
 		buildChecker('H', 'A', 8, out);
 	}
-	
+
 	public static void printSchachbrett(PrintWriter out){
 		buildChecker('A', 'H', 8, out);
 	}
-	
-	
+
+
 	public static void printSchachbrettReverse(PrintWriter out){
 		buildChecker('H', 'A', 8, out);
 	}
-	
+
 	public static void buildChecker(char fromLetter, char toLetter, int columns, PrintWriter out){
 		out.println("Checkerboard from " + fromLetter + " to " + toLetter + ":\n-------------------------");
-		
+
 		for(int delta = Math.abs(fromLetter - toLetter); delta >= 0; delta--){
 			char curLetter;
 			if(fromLetter > toLetter){
@@ -42,12 +42,12 @@ public class Schachbrett {
 				} else {
 				curLetter = (char)((int)toLetter - delta);
 			}
-			
+
 			for(int count = 1; count <= columns; count++){
 				out.print("" + curLetter + count + " ");
 			}
 			out.print("\n");
-			
+
 		}
 		out.println("\n");
 	}
--- src/pr1/a03/Aufgabe03.java
+++ src/pr1/a03/Aufgabe03.java
@@ -19,15 +19,19 @@ public class Aufgabe03 {
 	public static void writeMixedNumberFile(String filename, int numberCount, int width){
 		buildRandom(filename, numberCount, width);
 	}
-	
+
 	public static void buildRandom(String filename, int numberCount, int width){
 		DirtyFileWriter dfw = new DirtyFileWriter(filename);
 		PrintWriter out = new PrintWriter(dfw, false);
 		if(numberCount % 2 == 1) { numberCount ++; }
 
 		for(int i = 1; i <= numberCount; i++){
-			double randInt = 100.5 * Math.random();
-			out.printf("%" + width + "." + (width-4) + "f ", randInt);
+			double randDouble = 100.5 * Math.random();
+			if (Math.random() < 0.5) {
+				out.printf("%" + width + "d ", (int) randDouble);
+			} else {
+				out.printf("%" + width + "." + (width - 4) + "f ", randDouble);
+			}
 			if(i % 10 == 0) { out.println(); }
 		}
 		dfw.close();
