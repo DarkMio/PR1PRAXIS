b3e4c19cbb8b8f1a8b1038abe14690aa0311529c
DarkMio <miostarkid@gmail.com>
2015-04-19 13:05:14 +0200

    Fixing old software

--- src/pr2/a02/DateTest.java
+++ src/pr2/a02/DateTest.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class SimpleDateTest {
+public class DateTest {
 
     public static void main(String[] args) {
         try {
