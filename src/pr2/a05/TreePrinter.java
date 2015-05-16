ec4937ef06ca84ef0d6ed5c781c74cea1e310603
DarkMio <miostarkid@gmail.com>
2015-05-16 09:01:58 +0200

    a05 added, fixed, finished and cleaned up.

--- src/pr2/a05/TreePrinter.java
+++ src/pr2/a05/TreePrinter.java
@@ -5,21 +5,23 @@ import pr2.a04.TreeFactory;
 import schimkat.berlin.lernhilfe2015ss.tree.QadTree;
 
 import javax.swing.tree.TreeNode;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
 import java.util.Enumeration;
 import java.util.Iterator;
 
 public class TreePrinter {
 
     public static void main(String[] args){
-        QadTree tree = TreeFactory.createTree("./data/lib-tree/filetree.txt");
+        QadTree tree = TreeFactory.createTree("./data/lib-tree/tree-02.txt");
         PrintWriter out = new PrintWriter(System.out, true);
-        printTree(tree.postorderIterator(), out);
+        printTree(tree.postorderIterator(), out, 15, false);
         out.println("------------");
-        printTree(tree.preorderIterator(), out, 3, false);
+        printTree(tree.preorderIterator(), out, 15, false);
         out.println("------------");
-        printTree(tree.depthFirstIterator(), out);
+        printTree(tree.depthFirstIterator(), out, 15, false);
+        out.println("------------\nUnd nun die kür:\n-------------");
+        writeTreeBinary(tree, "./data/lib-tree/outputstream.txt");
+        printTree(readTreeBinary("./data/lib-tree/outputstream.txt").preorderIterator(), out, 15);
     }
 
     public static void printTree(Iterator<TreeNode> iterator, PrintWriter out) {
@@ -28,21 +30,50 @@ public class TreePrinter {
         }
     }
 
+    public static void printTree(Iterator<TreeNode> iterator, PrintWriter out, int width) {
+        printTree(iterator, out, width, false);
+    }
+
+    public static void writeTreeBinary(QadTree tree, String path) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path));
+            oos.writeObject(tree);
+        } catch (IOException e) {
+            System.err.println("File not found.");
+            System.exit(-1);
+        }
+    }
+
+    public static QadTree readTreeBinary(String path) {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
+            return (QadTree) ois.readObject();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(-1);
+        return new QadTree("");
+    }
+
     public static void printTree(Iterator<TreeNode> iterator, PrintWriter out, int width, boolean skipLeafs) {
         if (width <= 0) {
             return;
         }
         while (iterator.hasNext()) {
             TreeNode node = iterator.next();
-            out.print(node + " ");
+            out.printf("%-" + width + "s ", node);
             if (skipLeafs && node.isLeaf()) {
                 continue;
             }
             Enumeration enums = node.children();
-            if (enums.hasMoreElements()) {
-                System.out.println("! " + enums.nextElement());
+            while (enums.hasMoreElements()) {
+                out.printf("%-" + width + "s ", enums.nextElement());
             }
-            return;
+            out.println();
         }
     }
 }
